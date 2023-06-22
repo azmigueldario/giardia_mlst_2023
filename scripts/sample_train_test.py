792da9c86e36d611d1355dbfefad4f669abe7534
Miguel_Prieto <mprietog@sfu.ca>
2023-06-21 18:11:03 -0700

    created python script to divide datasets in test/training

--- scripts/sample_train_test.py
+++ scripts/sample_train_test.py
@@ -1,3 +1,64 @@
-import numpy as np 
+""" Script to produce n-fold cross validation datasets """
+
+# requires numpy, pandas, os, argparse and sklearn
+
+import os
+import argparse
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
+# user defined inputs
+directory = '../processed_data/cross_validation'
+n = 10 
+
+# define parsing of arguments from command line
+#def parse_arguments():
+"""Reads arguments from the command line for my script."""
+
+parser = argparse.ArgumentParser(description="Produces 'n' pairs of .csv files with testing and training datasets.")
+parser.add_argument("-n", "--number_iterations", metavar='N', default=10, 
+                    required=False, type=int,
+                    help='number of iterations for cross validation')
+parser.add_argument("-o", "--output_dir", metavar='', required=True, type=str,
+                    help='path [absolute/relative] to export output files')
+parser.add_argument("--test_proportion", default=0.2, type=float,
+                    help="percentage of available data to be used for testing.")
+parser.add_argument("input_file", 
+                    help="path [absolute/relative] to an input dataset in .csv format")
+
+args = parser.parse_args()
+print (args.test_proportion)
+
+
+# sanity check for arguments
+if args.test_proportion > 1 or args.test_proportion < 0:
+    raise ValueError('--test_proportion must be between 0 and 1.')
+
+
+def n_fold_split():
+    """ This function takes a '.csv' data file as input. It divides it into 'training' and 'testing' datasets
+        inside a for loop 'n' times. Every time a dataset is produced, the script saves it in the specified
+        output directory.             
+    """
+
+    # read dataset
+    df = pd.read_csv(r'../processed_data/input_samplesheet.csv',
+                header=0)
+
+    # create output_dir if it does not exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # divide in training and testing datasets n times
+    for i in range(1, (n+1)):
+        
+        train, test = train_test_split (df,
+                                        test_size=0.2)
+
+        train.to_csv(f'../processed_data/cross_validation/train_set_{i}.csv')
+        test.to_csv(f'../processed_data/cross_validation/test_set_{i}.csv')
+
+
+# run when script is called from CLI using 'python3 script.py'
+if __name__ == '__main__':
+    n_fold_split()
