68cbe1235dab313801fd8dae4c458464998b0a74
Miguel_Prieto <mprietog@sfu.ca>
2023-08-08 20:50:53 -0700

    updated custom config file for eagle

--- scripts/eagle/assembly_and_qc.sh
+++ scripts/eagle/assembly_and_qc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=3G
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=4
 #SBATCH --job-name="giardia_assembly_qc_full"
 #SBATCH --chdir=/scratch/mdprieto/
 #SBATCH --output=jobs_output/%x_%j.out
@@ -17,12 +17,14 @@ CUSTOM_CONFIG="/project/60006/mdprieto/giardia_mlst_2023/scripts/eagle/eagle.con
 
 ###############################################################################################
 
+    # eagle has low space available in scratch, save temp files in another temporary workdir
 nextflow run bactopia/bactopia -r v2.2.0 \
--profile singularity \
--resume \
---nfconfig $CUSTOM_CONFIG \
---samples $SAMPLESHEET \
---outdir results/bactopia_giardia \
---shovill_assembler spades \
---skip_amr
+    -profile singularity \
+    -resume \
+    -w /project/60006/mdprieto/nf_work_cache_giardia \
+    --nfconfig $CUSTOM_CONFIG \
+    --samples $SAMPLESHEET \
+    --outdir results/bactopia_giardia \
+    --shovill_assembler spades \
+    --skip_amr
 
