ad095a4d0473bb12fd1701d86967c6e2a21ce562
Miguel_Prieto <mprietog@sfu.ca>
2024-01-16 17:33:30 -0800

    updates assembly script (minor)

--- scripts/assembly_and_qc.sh
+++ scripts/assembly_and_qc.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #SBATCH --mem-per-cpu=3G
-<<<<<<< HEAD
 #SBATCH --time=96:00:00
-=======
-#SBATCH --time=56:00:00
->>>>>>> 8418fd8 (updates eagle config  with new profile and customized resources)
 #SBATCH --cpus-per-task=4
 #SBATCH --job-name="giardia_assembly_full"
 #SBATCH --chdir=/scratch/mdprieto/
@@ -33,11 +29,5 @@ nextflow run bactopia/bactopia -r v3.0.0 \
     --long_reads \
     --skip-prokka \
     --skip_mlst \
-<<<<<<< HEAD
     --cleanup_workdir 
-=======
-    --max_memory 90 \
-    --cleanup_workdir \
-    --slurm_queue 'cpubase_bycore_b1'
->>>>>>> 8418fd8 (updates eagle config  with new profile and customized resources)
 
