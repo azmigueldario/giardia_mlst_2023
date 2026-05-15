43627280e09428d3e6aa360b8dcb7227b767108d
mdprieto <azmigueldario@gmail.com>
2026-05-14 17:22:57 -0700

    Update: pilot run of chewbbacca pipeline

--- config_paths.sh
+++ config_paths.sh
@@ -31,14 +31,16 @@ elif [[ "$drac_cluster" == "fir" || "$current_host" == *"fir"* || "$current_host
     # MANUAL: define Fir-specific root paths
     project_root="/scratch/mdprieto/repositories/giardia_mlst_2023"
     scratch_tmp_folder="/scratch/mdprieto/cache"
+    nf_chewbbacca_pipeline="/scratch/mdprieto/repositories/nf_chewbbaca_mlst"
 
-    # MANUAL: container path(s)
+    # MANUAL: container path(s) and environments
     sra_tools_container="/project/6007413/cidgoh_share/singularity_imgs/sra-tools-3.2.1--h4304569_1.img"
     quast_container="/project/6007413/cidgoh_share/singularity_imgs/quast-5.0.2--py37pl526hb5aa323_2.img"
     sourmash_container="/project/6007413/cidgoh_share/singularity_imgs/sourmash-4.8.9--hdfd78af_0.img"
-
-    # MANUAL: python environment for clustering
     clustering_env="/home/mdprieto/virtual_envs/tsne_env2/"
+
+    # MANUAL: databases
+    eggnog_db_dir="/project/6007413/cidgoh_share/database/eagle/eggnog"
 
 # FALLBACK/UNKNOWN
 else
@@ -57,15 +59,17 @@ export sra_tools_container
 export quast_container
 export sourmash_container
 export clustering_env
+export eggnog_db_dir
 
 # AUTOMATED: relative input to primary subfolders
 export project_scripts_dir="${project_root}/scripts"
 export input_dir="${project_root}/input_data"
 export processed_data="${project_root}/processed_data"
 export project_outdir="${project_root}/output"
+export analysis_dir="${project_root}/analysis"
 
 # AUTOMATED: relative path to other subfolders
 export raw_reads_dir="${input_dir}/raw_fastq"
-export reference_genome="${input_dir}/reference_assemblage_A"
+export reference_genome_A="${input_dir}/reference_assemblage_A/GCF_000002435.2_WB_genomic.fna.gz"
 export accessions_dir="${processed_data}/accessions"
 export bactopia_results="${project_outdir}/bactopia_giardia_2025"
